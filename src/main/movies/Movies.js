f41eeb424ff73ecdc4f45dde08f291259658cded
Markus Tulin <macky_b@hotmail.com>
2018-09-25 22:41:06 +0200

    Added movies/Movies

---
+++ src/main/movies/Movies.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Movies.css";
+
+class Movies extends React.Component {
+  render() {
+    return (
+      <section className="movies">
+        Movies
+      </section>
+    )
+  }
+}
+
+export default Movies;
