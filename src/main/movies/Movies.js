7cd74030fe02c3ae32922d4ef79b558fd1b406c6
Markus Tulin <macky_b@hotmail.com>
2018-09-25 23:29:05 +0200

    Starting to display fetched movies

--- src/main/movies/Movies.js
+++ src/main/movies/Movies.js
@@ -2,18 +2,26 @@ import React from "react";
 import "./Movies.css";
 import MovieListItem from "./MovieListItem";
 
-const movies = ["Breaking bad", "Narcos", "Game of Thrones"];
-
 class Movies extends React.Component {
+  state = {
+    movies: []
+  }
+
   componentDidMount() {
     const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
 
     fetch(apiUrl)
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => this.storeMovies(data))
       .catch(error => console.log(error))
+  }
 
-    console.log("Before or after data?");
+  storeMovies = data => {
+    const movies = data.results.map(result => {
+      const  { vote_count, id, genre_ids, poster_path, title, vote_average, release_date } = result;
+      return { vote_count, id, genre_ids, poster_path, title, vote_average, release_date };
+    });
+    this.setState({movies})
   }
 
   render() {
@@ -21,8 +29,8 @@ class Movies extends React.Component {
       <section> 
         <ul className="movies">
           {
-            movies.map( (title, index) => (
-              <MovieListItem key={index.toString()} title={title} />
+            this.state.movies.map( movie => (
+              <MovieListItem key={movie.id} movie={movie} />
             ))
           }
         </ul>
