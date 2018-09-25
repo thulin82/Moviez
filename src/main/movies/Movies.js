5554cb16724d7a6a6fb14b69d70909b5eabf2ac5
Markus Tulin <macky_b@hotmail.com>
2018-09-25 23:05:33 +0200

    Started to use the API

--- src/main/movies/Movies.js
+++ src/main/movies/Movies.js
@@ -5,6 +5,17 @@ import MovieListItem from "./MovieListItem";
 const movies = ["Breaking bad", "Narcos", "Game of Thrones"];
 
 class Movies extends React.Component {
+  componentDidMount() {
+    const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error))
+
+    console.log("Before or after data?");
+  }
+
   render() {
     return (
       <section> 
