4b0d9f4a57e593fb424b3c4f45e74b7367cb6c8c
Markus Tulin <macky_b@hotmail.com>
2018-09-25 22:59:46 +0200

    Added MovieListItems

---
+++ src/main/movies/MovieListItem.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./MovieListItem.css";
+
+const MovieListItem = ({ title }) => (
+  <li className="movie-item">{title}</li>
+);
+
+export default MovieListItem;
--- src/main/movies/Movies.js
+++ src/main/movies/Movies.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./Movies.css";
+import MovieListItem from "./MovieListItem";
+
+const movies = ["Breaking bad", "Narcos", "Game of Thrones"];
 
 class Movies extends React.Component {
   render() {
     return (
-      <section className="movies">
-        Movies
+      <section> 
+        <ul className="movies">
+          {
+            movies.map( (title, index) => (
+              <MovieListItem key={index.toString()} title={title} />
+            ))
+          }
+        </ul>
       </section>
     )
   }
