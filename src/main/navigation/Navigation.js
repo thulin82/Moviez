fab8f5971dbf4704ce67670fd4bf8308b92196c9
Markus Tulin <macky_b@hotmail.com>
2018-09-26 20:45:27 +0200

    Adding react-input-range and sliders

--- src/main/navigation/Navigation.js
+++ src/main/navigation/Navigation.js
@@ -1,15 +1,46 @@
 import React from "react";
 import "./Navigation.css";
 import Selection from "./Selection";
+import Slider from "./Slider";
 
 class Navigation extends React.Component {
   state = {
-    genre: 'comedy'
+    genre: "comedy",
+    year: {
+      label: "year",
+      min: 1990,
+      max: 2017,
+      step: 1,
+      value: { min: 2000, max: 2017 }
+    },
+    rating: {
+      label: "rating",
+      min: 0,
+      max: 10,
+      step: 1,
+      value: { min: 8, max: 10 }
+    },
+    runtime: {
+      label: "runtime",
+      min: 0,
+      max: 300,
+      step: 15,
+      value: { min: 60, max: 120 }
+    }
   }
 
   onGenreChange = event => {
     this.setState({genre: event.target.value});
   }
+
+  onChange = data => {
+    this.setState({
+      [data.type]: {
+        ...this.state[data.type], // previous value of this.state.year
+        value: data.value         // overwrite the value property
+      }
+    });
+  };
 
   render() {
     return (
@@ -18,6 +49,10 @@ class Navigation extends React.Component {
           genre={this.state.genre}
           onGenreChange={this.state.onGenreChange}
         />
+        <Slider data={this.state.year} onChange={this.onChange} />
+        <Slider data={this.state.rating} onChange={this.onChange} />
+        <Slider data={this.state.runtime} onChange={this.onChange} />
+
       </section>
     )
   }
