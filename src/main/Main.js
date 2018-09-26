9ca9bc0b7aa0e4b3831e1510260ea1274499e9d3
Markus Tulin <macky_b@hotmail.com>
2018-09-26 21:36:08 +0200

    Move the state to the Main component

--- src/main/Main.js
+++ src/main/Main.js
@@ -3,11 +3,62 @@ import "./Main.css"
 import Navigation from "./navigation/Navigation";
 import Movies from "./movies/Movies";
 
+const genreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US`
+
 class Main extends React.Component {
+  state = {
+    url: genreUrl,
+    genre: "comedy",
+    genres: [],
+    year: {
+      label: "year",
+      min: 1990,
+      max: 2017,
+      step: 1,
+      value: { min: 2000, max: 2017 }
+    },
+    rating: {
+      label: "rating",
+      min: 0,
+      max: 10,
+      step: 1,
+      value: { min: 8, max: 10 }
+    },
+    runtime: {
+      label: "runtime",
+      min: 0,
+      max: 300,
+      step: 15,
+      value: { min: 60, max: 120 }
+    }
+  }
+
+  onGenreChange = event => {
+    this.setState({genre: event.target.value});
+  }
+
+  setGenres = genres => {
+    this.setState({genres});
+  }
+
+  onChange = data => {
+    this.setState({
+      [data.type]: {
+        ...this.state[data.type],
+        value: data.value
+      }
+    });
+  };
+
   render() {
     return (
       <section className="main">
-        <Navigation />
+        <Navigation 
+          onChange={this.onChange}
+          onGenreChange={this.onGenreChange}
+          setGenres={this.setGenres}
+          {...this.state}
+        />
         <Movies />
       </section>
     )
--- src/main/navigation/Selection.js
+++ src/main/navigation/Selection.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Selection.css"
 
-const Selection = ({genres, genre, onGenreChange }) => (
+const Selection = ({genre, genres, onGenreChange }) => (
     <div className="selection">
       <label>Genre</label>
       <select value={genre} onChange={onGenreChange}>
         {genres.map( genre => (
-          <option key={genre.id} value={genre.name}>{genre.name}</option>
+          <option value={genre.name} key={genre.id}>{genre.name}</option>
         ))}
       </select>
     </div>
