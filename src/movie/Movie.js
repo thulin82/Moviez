80e076284126a3677ba1c9f98e9ed2d878da2eac
Markus Tulin <macky_b@hotmail.com>
2018-10-01 20:54:32 +0200

    Added routes and endpoints

---
+++ src/movie/Movie.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Movie.css"
+
+class Movie extends React.Component {
+  render() {
+    return <div class="movie-page">Movie</div>;
+  }
+}
+
+export default Movie;
